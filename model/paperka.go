deab5a12d04b6516c64ad72d80bb707fd32fc96a
Rolly Maulana Awangga <awangga@gmail.com>
2025-03-09 16:00:42 +0700

    model user paperka

---
+++ model/paperka.go
@@ -0,0 +1,15 @@
+package model
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type UserResellerPaperka struct {
+	ID          primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty" query:"id" url:"_id,omitempty" reqHeader:"token"`
+	Nama        string             `json:"nama" bson:"nama"`
+	Alamat      string             `json:"alamat" bson:"alamat"`
+	Kelurahan   string             `json:"kelurahan" bson:"kelurahan"`
+	Kecamatan   string             `json:"kecamatan" bson:"kecamatan"`
+	Kota        string             `json:"kota" bson:"kota"`
+	Provinsi    string             `json:"provinsi" bson:"provinsi"`
+	Email       string             `json:"email" bson:"alamat"`
+	Phonenumber string             `json:"phonenumber" bson:"phonenumber"`
+}

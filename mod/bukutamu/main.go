451ba4d8cc7bcce0775c248ddba5e69d4552adbb
Rolly Maulana Awangga <awangga@gmail.com>
2025-04-07 15:31:36 +0700

    update logika

--- mod/bukutamu/main.go
+++ mod/bukutamu/main.go
@@ -9,19 +9,20 @@ import (
 )
 
 func BukuTamu(Profile model.Profile, Pesan model.WAMessage) (reply string) {
+	form := "https://wa.me/6281776900300?text=%2A8ukuT4muAth3na%2A+%0A%3E+Asal+%3A+%0A%3E+Tujuan+%3A+%0A%3E+NoPol+%3A+"
 	tamu := ParsePesanFlexible(Pesan.Message)
 	tamu.PhoneNumber = Pesan.Phone_number
 	tamu.Nama = Pesan.Alias_name
+	//cek dahulu isian asal dan nopol
+	if tamu.Kendaraan == "" || tamu.Kategori == "" {
+		return "Mohon maaf asal dan nopol harus diisi kak, silahkan coba kembali:\n" + form
+	}
 	//mengambil data satpam yang masuk shift aktif terakhir
 	satpam, err := mgdb.GetOneLatestDoc[model.Satpam](config.Mongoconn, "selfie", bson.M{})
 	if err != nil {
 		return "Mohon maaf saat ini belum ada satpam yang menjaga pos"
 	}
-	//mengambil data penghuni yang dituju
-	penghuniyangdikunjungi, err := mgdb.GetOneDoc[model.Penghuni](config.Mongoconn, "penghuni", bson.M{"nomorrumah": tamu.BlokRumah})
-	if err != nil && tamu.Tujuan != "" {
-		return "Mohon maaf tujuan yang kakak input tidak valid:\n_" + tamu.Tujuan + "_"
-	}
+
 	//pemberitahuan ke grup untuk ada yang jualan masuk ke komplek
 	if tamu.Tujuan == "niaga" {
 		notifgroup := model.TextMessage{
@@ -43,6 +44,11 @@ func BukuTamu(Profile model.Profile, Pesan model.WAMessage) (reply string) {
 		go jsonapi.PostStructWithToken[model.Response]("token", Profile.Token, notifsatpam, config.APIWAText)
 		mgdb.InsertOneDoc(config.Mongoconn, "bukutamu", tamu)
 		return "Selamat Kakak berhasil mengisi buku tamu dan terdaftar sebagai mitra pedagang athena"
+	}
+	//mengambil data penghuni yang dituju
+	penghuniyangdikunjungi, err := mgdb.GetOneDoc[model.Penghuni](config.Mongoconn, "penghuni", bson.M{"nomorrumah": tamu.BlokRumah})
+	if err != nil && tamu.Tujuan != "niaga" {
+		return "Mohon maaf tujuan yang kakak input tidak valid:\n_" + tamu.Tujuan + "_" + "\n silahkan coba kembali:\n" + form
 	}
 	//pemberitahuan ke satpam shift jaga
 	notifsatpam := model.TextMessage{
